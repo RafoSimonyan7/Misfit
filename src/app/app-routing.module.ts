1328567147c060c6fc4764db8db51ade68716e7b
RafoSimonyan7 <rafayelsimonyan1998@mail.ru>
2021-07-23 16:54:12 +0400

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+  },
+  {
+    path: 'gallery',
+    loadChildren: () => import('./gallery/gallery.module').then(m => m.GalleryModule)
+  },
+  {
+    path: 'news',
+    loadChildren: () => import('./news/news.module').then(m => m.NewsModule)
+  },
+  {
+    path: 'service',
+    loadChildren: () => import('./service/service.module').then(m => m.ServiceModule)
+  },
+  {
+    path: 'trainer',
+    loadChildren: () => import('./trainer/trainer.module').then(m => m.TrainerModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
